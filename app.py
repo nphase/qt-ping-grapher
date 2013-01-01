00dc9cb0f09db48746aa72996b07bb5399b3dd1d
Wilfried Schobeiri <ws@develtheory.com>
2013-01-01 16:08:05 -0600

    Fun with PyQT threads

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from PyQt4 import QtGui, QtCore, uic
 import os, sys, ping, time, rrd, signal
 
 host = "www.google.com"
@@ -7,7 +8,7 @@ rrdpng = "temp/ping.png"
 default_graph_width = 400
 default_graph_height = 100
 
-from PyQt4 import QtGui, QtCore, uic
+
 
 class HelloworldApp(QtGui.QMainWindow):
 	def __init__(self):
@@ -23,54 +24,60 @@ class HelloworldApp(QtGui.QMainWindow):
 		self.ui.show()
 		# self.ui.showFullScreen() #seems to be troublesome for raspberry pis
 		
+		rrd_thread.set_graph_size(self.ui.graph.size().width(), self.ui.graph.size().height())
 		
-		thread.set_graph_size(self.ui.graph.size().width(), self.ui.graph.size().height())
-		
-		self.connect(thread, thread.signal, self.updateUI)
+		self.connect(ping_thread, ping_thread.signal, self.updateUI)
 		self.connect(self.ui.exitButton, QtCore.SIGNAL("clicked()"), exit)
-		
 		
 	def updateUI(self, value):
 		win.ui.ms_label.setText(str(value) + " ms")
 		win.ui.graph.setPixmap(QtGui.QPixmap(os.getcwd() + "/" + rrdpng))
-	
-def exit():
-	sys.exit(0)
 
-class AThread(QtCore.QThread):
+
+class PingThread(QtCore.QThread):
 	def __init__(self):
 		QtCore.QThread.__init__(self, parent=app)
-		self.signal = QtCore.SIGNAL("signal")
-		self.graph_width = default_graph_width
-		self.graph_height = default_graph_height
-		
+		self.signal = QtCore.SIGNAL("ping_signal")
+	
 	def run(self):
-		rrd_count = 0
-		graph_reload_count = 0
 		
 		while True:
-			pingval = ping.ping(host)
+			try:
+				pingval = ping.ping(host)
+			except:
+				pingval = {'minping': -1, 'avgping': -1, 'timeout': True}
+			
 			rrd.update_rrd(rrdfile, pingval)
 			
-			if (rrd_count == 5):
-				rrd.update_png(rrdpng, rrdfile, self.graph_width, self.graph_height)
-				rrd_count = 0
-				graph_reload_count += 1
-			
-			if(graph_reload_count > 0):
-				self.emit( self.signal, str( round( float(pingval['minping']), 2 ) ) )
+			self.emit( rrd_thread.signal, pingval )
+			self.emit( self.signal, str( round( float(pingval['minping']), 2 ) ) )
 			
 			time.sleep(1)
-			rrd_count += 1
-			
+
+
+class RRDThread(QtCore.QThread):
+	def __init__(self):
+		QtCore.QThread.__init__(self, parent=app)
+		self.signal = QtCore.SIGNAL("rrd_png_update_signal")
+		self.graph_width = default_graph_width
+		self.graph_height = default_graph_height
+	
+	def run(self):
+		self.connect(ping_thread, rrd_thread.signal, self.update_png)
+		
+	def update_png(self, pingval):
+		rrd.update_png(rrdpng, rrdfile, self.graph_width, self.graph_height)
+	
 	def set_graph_size(self, width, height):
 		self.graph_width = int(width)
 		self.graph_height = int(height)
 
 
-def signal_handler(signal, frame):
+def exit():
 	sys.exit(0)
 
+def signal_handler(signal, frame):
+	sys.exit(0)
 
 if __name__ == "__main__":
 	signal.signal(signal.SIGINT, signal_handler)
@@ -80,7 +87,13 @@ if __name__ == "__main__":
 	
 	app = QtGui.QApplication(sys.argv)
 	app.setStyle('plastique')
-	thread = AThread()
+	
+	ping_thread = PingThread()
+	rrd_thread = RRDThread()
+	
 	win = HelloworldApp()
-	thread.start()
+	
+	ping_thread.start()
+	rrd_thread.start()
+	
 	sys.exit(app.exec_())
--- ping.py
+++ ping.py
@@ -3,9 +3,9 @@ import pingparser
 
 def ping(host):
 	ping = subprocess.Popen(
-	    ["ping", "-c", "1", host],
-	    stdout = subprocess.PIPE,
-	    stderr = subprocess.PIPE
+		["ping", "-c", "1", "-W", "1", host],
+		stdout = subprocess.PIPE,
+		stderr = subprocess.PIPE
 	)
 	
 	out, error = ping.communicate()
